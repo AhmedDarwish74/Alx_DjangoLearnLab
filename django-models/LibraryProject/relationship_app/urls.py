2061d81d3a6ed7ffb299439b36cd37bee2576aee
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-11-18 00:14:22 +0200

    new

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('list_books/', views.list_books, name='list_books'),  
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),  
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),  
+    path('register/', views.register, name='register'),   
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,9 @@ from .models import Book
 from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Function-based View to list all books
 def list_books(request):
@@ -13,3 +16,22 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  
     context_object_name = 'library'
+
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
