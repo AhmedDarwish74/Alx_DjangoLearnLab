a0bd7ce7317a2dee52dd7c12436fe4e4ac6e0eee
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-11-18 00:24:23 +0200

    new

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponse
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
 
 # Function-based View to list all books
 def list_books(request):
@@ -43,3 +45,30 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Welcome Admin!")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return HttpResponse("Welcome Librarian!")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("Welcome Member!")
