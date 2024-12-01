f60c1333036defe374d087c9ee00cc2c083bd40c
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-12-01 23:30:40 +0200

    edit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,17 @@
-from rest_framework.generics import ListAPIView  
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-class BookViewSet(viewsets.ModelViewSet.ListAPIView):
-    queryset = Book.objects.all()  
-    serializer_class = BookSerializer  
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
-
-
-
+class BookList(APIView):
+    def get(self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
