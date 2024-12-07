662f6ece7570789b8f0e4275dfc8ec84a151de2d
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-12-07 20:11:07 +0200

    edit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm
+from django.contrib.auth import update_session_auth_hash
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect('login')  
+    else:
+        form = RegistrationForm()
+    return render(request, 'auth/register.html', {'form': form})
+
+
+
+@login_required
+def profile(request):
+    """Showing users data"""
+    if request.method == 'POST':
+     
+        request.user.email = request.POST.get('email')
+        request.user.save()
+        return redirect('profile')
+    
+    return render(request, 'auth/profile.html', {'user': request.user})
