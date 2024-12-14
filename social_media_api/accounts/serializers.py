302412f85a278ad0df730f2a6429d038580bb793
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-12-14 18:06:57 +0200

    edit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import update_last_login
 
-# Use get_user_model() to dynamically reference the custom user model
+
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -14,11 +14,9 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        password = validated_data.pop('password')  # Extract password from the validated data
-        
-        # Create the user using create_user to ensure password is hashed
+        password = validated_data.pop('password')  
         user = User.objects.create_user(**validated_data)
-        user.set_password(password)  # Explicitly set the password to hash it
+        user.set_password(password)  
         user.save()
 
         # Create and return the token for the user
@@ -33,10 +31,9 @@ class UserLoginSerializer(serializers.Serializer):
         from django.contrib.auth import authenticate
         user = authenticate(**data)
         
-        # If the user is authenticated and active, proceed
         if user and user.is_active:
             update_last_login(None, user)
             return user
         
-        # Raise a validation error if authentication fails
+        
         raise serializers.ValidationError("Invalid credentials")
