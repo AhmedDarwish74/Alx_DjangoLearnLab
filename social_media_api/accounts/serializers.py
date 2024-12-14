4d5a9feb62550b9a76659b32c67959d8a0f6f989
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-12-14 17:24:00 +0200

    edit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,32 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import CustomUser
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import update_last_login
-from rest_framework.authtoken.models import Token
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'password', 'email', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
-        Token.objects.create(user=user)
-        return user
-
-class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-
-    def validate(self, data):
-        from django.contrib.auth import authenticate
-        user = authenticate(**data)
-        if user and user.is_active:
-            update_last_login(None, user)
-            return user
-        raise serializers.ValidationError("Invalid credentials")
-    
+# Use get_user_model() to dynamically reference the custom user model
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -37,14 +14,18 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        # Extract password from validated data
-        password = validated_data.pop('password')
-        
-        # Create the user using create_user() to ensure password is hashed
-        user = User.objects.create_user(**validated_data)
-        
-        # Set the password explicitly after creating the user to ensure it is hashed
-        user.set_password(password)
-        user.save()
-        
+        password = validated_data.pop('password')  # Extract password
+        user = User.objects.create_user(**validated_data)  # Create user with password hashing
+        user.save()  # User is already saved with hashed password by create_user
         return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            update_last_login(None, user)  # Update last login time
+            return user
+        raise serializers.ValidationError("Invalid credentials")
